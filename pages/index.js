c0145e39f658b8919ab68728314792b8b40ae076
Pau <paumrch94@gmail.com>
2021-07-21 12:55:34 +0200

    sm

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
       <Header />
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center pb-16">
-        <h1 className="text-6xl font-main font-bold">
+        <h1 className="text-6xl font-main font-bold sm:font-main">
           Hola a {' '}
           <a className="text-red-300" href="/">
             Cremona
@@ -56,7 +56,7 @@ export default function Home() {
 
         <p className="mt-3 text-2xl">
           La casa de los jóvenes {' '}
-          <code className="p-3 font-main font-bold font-bold text-lg bg-red-100 rounded-md">
+          <code className="p-3 font-main font-regular sm:font-main text-lg bg-red-100 rounded-md">
             escritores
           </code>.
         </p>
